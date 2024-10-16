93bbfd0a95cae248e7b8e7b4f5a325d28d4bc412
Abubakar Imoro <abubakarimoro27@gmail.com>
2024-10-16 11:52:23 +0000

    middlewares and validators added

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,28 @@
-export const registerUser = (req, res, next) => {
-    res.json('user registered successfully');
+import { UserModel } from "../models/user.js";
+import { registerUserValidator } from "../validators/user.js";
+import { hashSync } from "bcryptjs";
+
+
+export const registerUser = async (req, res, next) => {
+try {
+        // validate user input
+    const { error, value } = registerUserValidator.validate(req.body);
+    if (error) {
+        return res.status(422).json(error);
+    }
+        // check if user does not exist
+    const user = await UserModel.findOne({ email: value.email });
+    if (user) {
+        return res.status(409).json('user already exists!');
+    }
+        // hash their password
+        // save user to database  
+        // send user confirmation email
+        // respond to requests 
+        res.json('user registered successfully');
+} catch (error) {
+   next(error); 
+}
 }
 export const loginUser = (req, res, next) => {
     res.json('user logged in');
---
+++ middleware/upload.js
@@ -0,0 +1,18 @@
+import multer from "multer";
+import { multerSaveFilesOrg } from "multer-savefilesorg";
+
+export const todoIconUpload = multer({
+    storage:multerSaveFilesOrg({
+    apiAccessToken: process.env.SAVEFILESORG_API_KEY,
+    relativePath: '/todo-api/todos/*'    
+    }),
+    preservePath: true   
+});
+
+export const userAvatarUpload = multer({
+    storage:multerSaveFilesOrg({
+    apiAccessToken: process.env.SAVEFILESORG_API_KEY,
+    relativePath: '/todo-api/users/*'    
+    }),
+    preservePath: true   
+});
